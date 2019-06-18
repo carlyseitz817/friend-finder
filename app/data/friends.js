8f370dbf760814266cc35239cf489c73c7aad1cf
Carly Seitz <carlyseitz817@gmail.com>
2019-06-17 22:11:56 -0500

    responsive survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,24 @@
 var friends = [
     {
-      name: "Samantha",
-      photo: "https://photos.peterhurley.com/sites/default/files/styles/large/public/news/wp-content/uploads/2012/03/cali_0001web.jpg?itok=9PuWN986",
-      scores: ['2', '3', '4', '5', '2', '1', '1', '1', '3', '1']
+        name: "Samantha",
+        photo: "https://photos.peterhurley.com/sites/default/files/styles/large/public/news/wp-content/uploads/2012/03/cali_0001web.jpg?itok=9PuWN986",
+        scores: ['2', '3', '4', '5', '2', '1', '1', '1', '3', '1']
     },
-      {
-      name: "Ted",
-      photo: "https://thewondrous.com/wp-content/uploads/2015/05/black-people-funny-pictures.jpg",
-      scores: ['4', '3', '4', '5', '5', '5', '3', '2', '3', '1']
+    {
+        name: "Ted",
+        photo: "https://thewondrous.com/wp-content/uploads/2015/05/black-people-funny-pictures.jpg",
+        scores: ['4', '3', '4', '5', '5', '5', '3', '2', '3', '1']
     },
-      {
-      name: "Elizabeth",
-      photo: "https://i2-prod.mirror.co.uk/incoming/article5428573.ece/ALTERNATES/s615b/archetypal-female-_3249633c.jpg",
-      scores: ['5', '1', '5', '1', '1', '5', '4', '3', '3', '3']
+    {
+        name: "Elizabeth",
+        photo: "https://i2-prod.mirror.co.uk/incoming/article5428573.ece/ALTERNATES/s615b/archetypal-female-_3249633c.jpg",
+        scores: ['5', '1', '5', '1', '1', '5', '4', '3', '3', '3']
     },
-      {
-      name: "Craig",
-      photo: "https://unit22portraitphotographyjosh.files.wordpress.com/2014/09/cropped-7220468940_2ee9702a23_z.jpg",
-      scores: ['5', '5', '4', '1', '2', '3', '5', '5', '2', '4']
+    {
+        name: "Craig",
+        photo: "https://unit22portraitphotographyjosh.files.wordpress.com/2014/09/cropped-7220468940_2ee9702a23_z.jpg",
+        scores: ['5', '5', '4', '1', '2', '3', '5', '5', '2', '4']
     }
-  ];
-  
-  module.exports = friends;
+];
+
+module.exports = friends;
